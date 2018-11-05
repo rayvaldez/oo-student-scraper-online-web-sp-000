cb414ead0b1d5788fc616a0dd224f9501041ddab
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-05 16:24:51 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,22 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_html = open(index_url)
+    index_doc = Nokogiri::HTML(index_html)
+    student_cards = index_doc.css(".student-card")
+    students = []
+    student_cards.collect do |student_card_xml|
+      students << {
+        :name => student_card_xml.css("h4.student-name").text,
+        :location => student_card_xml.css("p.student-location").text,
+        :profile_url => "./fixtures/student-site/" + student_card_xml.css("a").attribute("href").value
+        }
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
