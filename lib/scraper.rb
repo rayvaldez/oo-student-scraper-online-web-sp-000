bc326884661058a86e95af9cd23eb5ee8e71a669
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-05 16:28:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     doc.css(".student-card").collect do |student|
-    student_hash = {
-    name: student.css("h4.student-name").text,
-    location: student.css("p.student-location").text,
-    profile_url: student.css("a").attr("href").value
-    }
+      student_hash = {
+      name: student.css("h4.student-name").text,
+      location: student.css("p.student-location").text,
+      profile_url: student.css("a").attr("href").value
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
