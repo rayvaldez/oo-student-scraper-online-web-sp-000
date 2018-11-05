953823ef174ca0439097a96cca07cb957ccf5865
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-05 16:42:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do {|k,v| self.send(("#{k}="), v)}
+    student_hash.each do {|key, value| self.send(("#{key}="), value)}
     #takes in an argument of a hash and sets that new students attributes
     #using the key/value paurs of that hash
     binding.pry
